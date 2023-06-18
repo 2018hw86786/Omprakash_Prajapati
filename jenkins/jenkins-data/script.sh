055134ac7e716a195fdbbc9dfcd4d8fe4e7c0db2
2018hw86786 <2018hw86786@wilp.bits-pilani.ac.in>
2023-06-18 23:14:41 +0530

    dsf

--- jenkins/jenkins-data/script.sh
+++ jenkins/jenkins-data/script.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
-# NAME=$1
-# LASTNAME=$2
-# SHOW=$3
+!/bin/bash
+NAME=$1
+LASTNAME=$2
+SHOW=$3
 
-# if [ "$SHOW" = 'true' ]; then
-# 	echo "hello, $NAME $LASTNAME"
-# else
-# 	echo "If you wannat see the name, then please mark the show option"
-# fi
-FIRST_NAME=$1
-SECOND_NAME=$2
-LAST=$3
-echo "hello $FIRST_NAME $SECOND_NAME $LAST"
+if [ "$SHOW" = 'true' ]; then
+	echo "hello, $NAME $LASTNAME"
+else
+	echo "If you wannat see the name, then please mark the show option"
+fi
+# FIRST_NAME=$1
+# SECOND_NAME=$2
+# LAST=$3
+# echo "hello $FIRST_NAME $SECOND_NAME $LAST"
