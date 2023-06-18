10b0b013af364cf8f1032fe953587c8abaf41988
2018hw86786 <2018hw86786@wilp.bits-pilani.ac.in>
2023-06-18 22:43:16 +0530

    sd

--- jenkins/jenkins-data/script.sh
+++ jenkins/jenkins-data/script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-NAME=$1
-LASTNAME=$2
-SHOW=$3
+# NAME=$1
+# LASTNAME=$2
+# SHOW=$3
 
-if [ "$SHOW" = 'true' ]; then
-	echo "hello, $NAME $LASTNAME"
-else
-	echo "If you wannat see the name, then please mark the show option"
-fi
+# if [ "$SHOW" = 'true' ]; then
+# 	echo "hello, $NAME $LASTNAME"
+# else
+# 	echo "If you wannat see the name, then please mark the show option"
+# fi
+
+echo "hello Omprakash Prajapati"
